e2bffedf72fd7e728909bfd85c96bba4fe2c0e23
ayushkatare <ayushkatare123@gmail.com>
2021-10-05 10:08:30 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootgradledefaulttemplate/SpringBootGradleDefaultTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradledefaulttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleDefaultTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleDefaultTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootgradledefaulttemplate/SpringBootGradleDefaultTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradledefaulttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleDefaultTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
